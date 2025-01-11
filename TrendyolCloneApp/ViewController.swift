0ac74f445aa3471d64e1b3c51e2981d6d1065d11
busraerim <busraeriim.4@gmail.com>
2025-01-11 12:40:01 +0300

    layout refactor

--- TrendyolCloneApp/ViewController.swift
+++ TrendyolCloneApp/ViewController.swift
@@ -142,7 +142,7 @@ extension ViewController {
         
         let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.3), heightDimension: .fractionalHeight(1))
         let item = NSCollectionLayoutItem(layoutSize: itemSize)
-        item.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 0, bottom: 10, trailing: 0)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
         
         let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.9),heightDimension: .fractionalHeight(0.15))
         let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 4)
